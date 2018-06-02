b2586e8e60ad2468fdb768ac677251c4ff8123d3
Ritika Mehta <mehtarit@umich.edu>
2018-06-02 17:18:42 -0500

    Added react router dom

---
+++ src/components/Results/Results.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="Results">
+            <p>REsults</p>
+            </div>
+        );
+    }
+}
+
+export default Results;
