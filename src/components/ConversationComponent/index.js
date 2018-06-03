26a77e82b39c372416fbc5eed69b16b96c3cc7c9
Adam <davidatomhernandez@gmail.com>
2018-06-02 21:43:10 -0500

    add component

---
+++ src/components/ConversationComponent/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const ConversationComponent = (props) => {
+  return (
+    <div>
+      <h1> convo compnent </h1>
+    </div>
+  )
+}
+
+export default ConversationComponent
