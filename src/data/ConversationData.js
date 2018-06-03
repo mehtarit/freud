c913591adc9ab7804e8e6b483d5f7bff7dcd55a0
Jeremy Cantu <mail@jeremycantu.com>
2018-06-02 21:40:57 -0500

    Conversations data and API commit

---
+++ src/data/ConversationData.js
@@ -0,0 +1,18 @@
+const conversations = [{
+    id: 0,
+    type: "radio",
+    prompt: "Would you prefer a therapist that is...",
+    answers: ["Female", "Male", "I am not sure yet"]
+}, {
+    id: 1,
+    type: "checkbox",
+    prompt: "Which type(s) of therapy would you consider?",
+    answers: ["One on One", "Group session", "Online therapy e.g. web chat, Skype", "Phone counseling"]
+}, {
+    id: 2,
+    type: "radio",
+    prompt: "To improve your match, please tell us your age.",
+    answers: ["Under 18", "18-25", "26-35", "36-49", "50+"]
+}, ]
+
+export default conversations;
