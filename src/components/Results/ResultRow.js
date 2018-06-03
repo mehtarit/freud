5c468bffc7fa7755ebe6aa75075bc032a0d9e137
Jeremy Cantu <mail@jeremycantu.com>
2018-06-03 11:42:13 -0500

    Results page minor updates

--- src/components/Results/ResultRow.js
+++ src/components/Results/ResultRow.js
@@ -3,22 +3,31 @@ import React, { Component } from 'react';
 import ResultTag from './ResultTag'
 
 import AfflictionsApi from '../../api/AfflictionsApi'
+import ResourcesApi from '../../api/ResourcesApi'
 
 class ResultRow extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            afflictions: AfflictionsApi.getAll()
+            afflictions: AfflictionsApi.getAll(),
+            resources: ResourcesApi.getAll()
         };
     }
 
     render() {
         return (
             <div className="ResultRow">
-                <div className="AfflictionCardWrapper">{this.state.afflictions.map((affliction, index) => (
+                <div className="AfflictionsTagsWrapper">{this.state.afflictions.map((affliction, index) => (
                     <ResultTag
                         key={index}
                         label={affliction.prompt} />
+                ))
+                }</div>
+                <hr />
+                <div className="ResourcessTagsWrapper">{this.state.resources.map((resource, index) => (
+                    <ResultTag
+                        key={index}
+                        label={resource.resourceTitle} />
                 ))
                 }</div>
                 <hr />
