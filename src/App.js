ae3dd84d04e8564204116fee92c90f4acd8d407e
Ritika Mehta <mehtarit@umich.edu>
2018-06-03 10:08:23 -0500

    Modified App.js to read from ConvesationApi

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import ConversationApi from './api/ConversationApi';
 
 import Main from './Main'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      conversations: ConversationApi.getAll()
+    };
   }
 
+
   render() {
-    const dynamicQuestions = [
-      {
-        type: 'checkbox',
-        prompt: 'How are you feeling?',
-        answers: ['goooooood', 'bad', 'great'],
-      },
-      {
-        type: 'radio',
-        prompt: 'How are you feeling?',
-        answers: ['good', 'bad', 'gt'],
-      },
-      {
-        type: 'input',
-        prompt: 'How are you feeling?',
-        answers: ['go', 'bad', 'great'],
-      }
-    ];
     return (
       <div className="App">
         <Main
-          dynamicQuestions={dynamicQuestions}
+          dynamicQuestions={ConversationApi.getAll()}
         />
       </div>
     );
   }
 }
+
 
 export default App;
