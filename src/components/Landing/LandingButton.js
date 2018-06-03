6d7812d956228d68eb9ae4d272d0da733a10b8b3
Jeremy Cantu <mail@jeremycantu.com>
2018-06-03 13:52:07 -0500

    Landing button styling

--- src/components/Landing/LandingButton.js
+++ src/components/Landing/LandingButton.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-class LandingButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+const styles = {
+    button: {
+        backgroundColor: '#DE5B49',
+        borderRadius: 3,
+        border: 0,
+        color: '#fff',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+    },
+};
 
-    render() {
-        return (
-            <Button variant="raised" color="primary">{this.props.buttonText}</Button>
-        );
-    }
+function LandingButton(props) {
+    return (
+        <Button variant="raised" className={props.classes.button}>{props.buttonText}</Button>
+    );
 }
 
 LandingButton.propTypes = {
-    buttonText: PropTypes.string
-}
+    buttonText: PropTypes.string,
+    children: PropTypes.node,
+    classes: PropTypes.object.isRequired,
+};
 
-export default LandingButton;
+export default withStyles(styles)(LandingButton);
+
