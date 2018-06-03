3b046b96c40bd4864eaa19cd3251a07b2c99f6b9
Ritika Mehta <mehtarit@umich.edu>
2018-06-02 19:34:35 -0500

    Added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import Header from './Header'
+import Main from './Main'
 import './styles/App.css';
-
 import Landing from './components/Landing/Landing'
+
 
 class App extends Component {
   constructor(props) {
@@ -13,9 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <Landing />
+        <Header />
+        <Main />
       </div>
     );
   }
 }
 
 export default App;
+
