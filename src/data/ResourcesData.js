f2aee19ac549ad07f791cd39187d9afa1cc295be
Jeremy Cantu <mail@jeremycantu.com>
2018-06-03 10:15:38 -0500

    Added links to resources images to data-set

--- src/data/ResourcesData.js
+++ src/data/ResourcesData.js
@@ -1,31 +1,31 @@
 const resources = [{
     id: 0,
-    "imageURL": "http://cristinaculiolopeck.com/img/rs-slider/slides/cristina-slide.png",
+    "imageURL": "https://images.agoramedia.com/everydayhealth/gcms/cs-asthma-support-groups-manage-1440x810.jpg?width=722",
     resourceTitle: "Support Groups",
     resourceSubtext: "In a support group, members provide each other with various types of help, usually nonprofessional, for a particular shared, usually burdensome, characteristic. Members with the same issues can come together for sharing coping strategies, to feel more empowered and for a sense of community. Support groups provide relevant information, relating personal experiences, listening to and accepting others' experiences, providing sympathetic understanding and establishing social networks."
 }, {
     id: 1,
-    "imageURL": "http://cristinaculiolopeck.com/img/rs-slider/slides/cristina-slide.png",
+    "imageURL": "https://www.goodtherapy.org/blog/blog/wp-content/uploads/2015/12/Therapist-comforting-person-in-therapy.jpg",
     resourceTitle: "A Counselor",
     resourceSubtext: "Mental Health Counselors work with individuals, couples, families, and groups to address and treat emotional and mental disorders and to promote mental health.Most mental health counselors in the U.S.work in outpatient and residential care centers, individual and family services, and local governments.They are trained in a variety of therapeutic techniques used to address issues, including depression, anxiety, addiction and substance abuse, suicidal impulses, stress, problems with self - esteem, and grief."
 }, {
     id: 2,
-    "imageURL": "http://cristinaculiolopeck.com/img/rs-slider/slides/cristina-slide.png",
+    "imageURL": "https://chrismercer.net/wp-content/uploads/2016/10/ZCM.10.20.16.jpg",
     resourceTitle: "Legal Assistance",
     resourceSubtext: "If you need legal help with a mental health hearing or appeal, speak with the patient advocate or rights advisor in the facility where you are admitted or receiving treatment.The patient advocate or rights advisor can advise you of your rights and help you choose a lawyer if one is needed."
 }, {
     id: 3,
-    "imageURL": "http://cristinaculiolopeck.com/img/rs-slider/slides/cristina-slide.png",
+    "imageURL": "http://arabcooking.me/wp-content/uploads/download-map-uc-health-midtown-major-tourist-attractions-maps-inside.jpg",
     resourceTitle: "Crisis Centers",
     resourceSubtext: "Crisis centers are the ultimate \"community mental health service,\" as they usually serve the entire community, often at all hours and free of charge. Smaller centers serve a few thousand people each year, while others serve hundreds of thousands annually, depending on their community’ s size and the strength of their funding sources.They are a also a vital resource to their community service systems by connecting callers to providers that exist to care for their needs."
 }, {
     id: 4,
     "imageURL": "http://cristinaculiolopeck.com/img/rs-slider/slides/cristina-slide.png",
-    resourceTitle: "Legal Assistance",
+    resourceTitle: "Psychiatrist",
     resourceSubtext: "Psychiatrist provide therapy, but they can also prescribe medication and diagnose medical illnesses like diabetes or high blood pressure.Psychiatrists are also able to determine the effects a mental condition can have on other medical issues such as heart disease, obesity, and alcoholism."
 }, {
     id: 5,
-    "imageURL": "http://cristinaculiolopeck.com/img/rs-slider/slides/cristina-slide.png",
+    "imageURL": "https://gem4meinvestments.eu/assets/img/img13.jpeg",
     resourceTitle: "Fact and Information",
     resourceSubtext: "Take advantage of a database of information and resources."
 }, ]
